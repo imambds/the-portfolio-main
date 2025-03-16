5b5f2c0f60374fcd7b2bb0f0f7c0b7cc000b5bba
Imam Budi Prakoso <94958797+imambds@users.noreply.github.com>
2025-03-17 01:23:23 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,14 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static"; // <== ini wajib!
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
   };
 }
