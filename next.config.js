2429673674ffe237c259d4788300f9d9a96f38f9
Imam Budi Prakoso <94958797+imambds@users.noreply.github.com>
2025-03-17 01:13:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: "portfolio-image-store.s3.ap-south-1.amazonaws.com",
-      },
-      {
-        hostname: "res.cloudinary.com",
-      },
-    ],
-  },
-};
+  output: 'export',
+}
 
-export default nextConfig;
+module.exports = nextConfig
