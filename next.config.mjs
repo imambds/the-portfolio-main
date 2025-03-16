4d9c677f47d44d247b70d38304453132b1ae7c93
Imam Budi Prakoso <94958797+imambds@users.noreply.github.com>
2025-03-17 00:47:16 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        hostname: "portfolio-image-store.s3.ap-south-1.amazonaws.com",
-      },
-      {
-        hostname: "res.cloudinary.com",
-      },
-    ],
-  },
-};
+  output: 'export',
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
